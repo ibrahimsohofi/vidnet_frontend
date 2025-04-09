77ae18152a9242116ac1d06f3f885ea3cdeba54a
ibrahim sohofi <78817932+ibrahimsohofi@users.noreply.github.com>
2025-04-09 16:37:01 +0100

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -76,7 +76,7 @@ function Header() {
       const fetchData = async () => {
         try {
           const response = await axios.get(
-            `https://4f29-34-27-45-175.ngrok-free.app/video-info?url=${encodeURIComponent(
+            `https://0bcc-34-27-45-175.ngrok-free.app/video-info?url=${encodeURIComponent(
               inputValue
             )}`,
             {
